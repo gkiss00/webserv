2d9faee5a55e5842ea35c7d060b1bab411cbb5d6
agossuin <agossuin@student.s19.be>
2021-02-07 18:41:09 +0100

    fixed what i broke... queue-

--- srcs/main/MyWebServer.cpp
+++ srcs/main/MyWebServer.cpp
@@ -1,4 +1,5 @@
 #include "MyWebServer.hpp"
+#define MAX_CLIENT
 
 // Based on https://www.csd.uoc.gr/~hy556/material/tutorials/cs556-3rd-tutorial.pdf
 // https://www.tenouk.com/Module41a.html
@@ -9,48 +10,56 @@ MyWebServer::MyWebServer(std::string config_path) {
     this->_bind_all("127.0.0.1");
 }
 
-MyWebServer::~MyWebServer() {}
+MyWebServer::~MyWebServer() {
+
+    // Close the servers
+    for (size_t i = 0; i < servers.size(); ++i)
+        close(servers[i].socket);
+
+    // Close the clients
+    while (queue_clients.size()) {
+        close(queue_clients.front());
+        client_server.erase(queue_clients.front());
+        FD_CLR(queue_clients.front(), &current_sockets);
+        queue_clients.pop_front();
+    }
+}
 
 // SOCKET + BIND
-void    MyWebServer::close_socket (const char *error_msg, int sock) {
+void    MyWebServer::error_socket (const char *error_msg, int sock) {
     close (sock);
-    perror (error_msg);
+    std::cout << "\e[31m" << error_msg << " failed\e[m" << std::endl;
 }
 
 int    MyWebServer::new_socket(const char *host, int port) {
     int                 sock;
     struct sockaddr_in  addrServer;
 
-    bzero(&addrServer, sizeof(addrServer));
     //fill the struct the server socket will refere to
     addrServer.sin_addr.s_addr = inet_addr(host);     //HOST
-    addrServer.sin_family = PF_INET;                  //IPV4
+    addrServer.sin_family = AF_INET;                  //IPV4
     addrServer.sin_port = htons(port);                //PORT
 
-    // if ((sock = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
-    if ((sock = socket (PF_INET, SOCK_STREAM, 0)) == -1)
-        perror ("socket");
+    if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
+        error_socket ("socket", -1);
 
     int option = 1;
-    if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &option, sizeof(int)) == -1)
-        close_socket ("setsockopt SO_REUSEADDR", sock);
-    option = 1;
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(int)) == -1)
-        close_socket ("setsockopt SO_REUSEADDR", sock);
-    // int size_buf = 1000;
-    // if (setsockopt (sock, SOL_SOCKET, SO_SNDBUF, &size_buf, sizeof (int)) == -1)
-    //     close_socket ("setsockopt SO_SNDBUF", sock);
+        error_socket ("setsockopt SO_REUSEADDR", sock);
 
     //bind the server socket with the struct
     if (bind(sock, (const struct sockaddr *)&(addrServer), sizeof(addrServer)) == -1)
-        close_socket ("bind", sock);
+        error_socket ("bind", sock);
     std::cout << "Bind #" << sock << ", port #" << port << std::endl;
-    if (fcntl(sock, F_SETFL, O_NONBLOCK) == -1)
-        close_socket ("Non blocking", sock);
-    // if (fcntl(sock, F_GETFL, O_NONBLOCK) == -1)
-    //     close_socket ("Non blocking", sock);
 
-    listen(sock, 1000);
+    // NONBLOCK
+    if (fcntl(sock, F_SETFL, O_NONBLOCK) == -1)
+        error_socket ("Non blocking", sock);
+
+    // Listen
+    if (listen(sock, 1000) == -1)
+        error_socket ("Listen", sock); // 1000 queue accepted
+    
     this->server_sockets.push_back(sock);
     return sock;
 }
@@ -115,25 +124,27 @@ int     MyWebServer::accept_client(int server_sock) {
 
     std::cout << "client #" << client_sock << " connected to #" << server_sock << std::endl;
 
-    // add client to queue / map
+    // Add client to queue / map
     client_server[client_sock] = server_sock;
-    queue_clients.push(client_sock);
+    queue_clients.push_back(client_sock);
 
     // suppress first client if too much of them
     if (this->queue_clients.size() > 200) // limit the amount of clients
-    {
-        close(queue_clients.front());
-        client_server.erase(queue_clients.front());
-        FD_CLR(queue_clients.front(), &current_sockets);
-        queue_clients.pop();
-    }
+        this->remove_client(queue_clients.front());
 
     return client_sock;
 }
 
+void     MyWebServer::remove_client(int client_sock) {
+    close(client_sock);
+    queue_clients.remove(client_sock);
+    client_server.erase(client_sock);
+    FD_CLR(client_sock, &current_sockets);
+}
+
+
 Server &MyWebServer::server_from_fd(int fd) {
-    for (size_t i = 0; i < servers.size(); ++i)
-    {
+    for (size_t i = 0; i < servers.size(); ++i) {
         if (servers[i].socket == fd) return servers[i];
     }
     return servers[0];
@@ -143,7 +154,6 @@ Server &MyWebServer::server_from_fd(int fd) {
 // RUN
 void    MyWebServer::run() {
     while (true) {
-        std::map<int, int>::iterator it;
 
         // set current_sockets for select
         int max = 0;
@@ -153,64 +163,47 @@ void    MyWebServer::run() {
             FD_SET(servers[i].socket, &current_sockets);
             if (servers[i].socket > max) max = servers[i].socket;
         }
-        for (it = client_server.begin(); it != client_server.end(); ++it)
+        for (std::map<int, int>::iterator it = client_server.begin(); it != client_server.end(); ++it)
         {
             FD_SET(it->first, &current_sockets);
             if (it->first > max) max = it->first;
         }
 
-        // because select is destructive
-        // struct timeval tv = {2, 0};
+        // SELECT
         if (select(max + 1, &current_sockets, NULL, NULL, NULL) == -1) {
             std::cout << "select failed" << std::endl;
             break ;
         }
 
-        // Check for internal
+        // Go through fds
         for (int fd = 0; fd <= max; ++fd) {
 
-            //if the fd is in the set
+            // If the fd is in the set
             if (FD_ISSET(fd, &current_sockets)) {
                 
                 // ADD THE CLIENT
                 if (std::count(this->server_sockets.begin(), this->server_sockets.end(), fd)) {
                     accept_client(fd);
+                    FD_CLR(fd, &current_sockets);
                 }
 
                 // HANDLE THE CLIENT
                 else {
-                    try {
-                        // clock_t time_req = clock(); // TIME_REQUIRED
-                        
+                    try {                        
                         std::string content = _recv(fd);
                         if (content != "")
                         {
-                            // BLU(clock() - time_req) // TIME TO RECEIVE BLUE
-
-                            // time_req = clock(); // TIME_REQUIRED
                             RequestParser   request(content);
-                            // YLW(clock() - time_req) // TIME TO PARSE YLW
-                        
-                            // time_req = clock(); // TIME_REQUIRED
                             Response response(request, server_from_fd(client_server[fd]));                            
                             std::string response_render(response.render());
-                            // GRN(clock() - time_req) // TIME TO ANSWER GRN
-
-                            // time_req = clock(); // TIME_REQUIRED
 
                             _send(fd, response_render);
                             FD_CLR(fd, &current_sockets);
-                            if (request.command == "GET" && content.size() != 95) {
-                                YLW(content.size())
-                            }
                             if (request.command == "PUT"){
-                                if (close(fd) < 0)
-                                    perror("closing client");
-                                client_server.erase(fd);
+                                remove_client(fd);
                             }
                         }
                     } catch(request_exception &e) {
-                        // bad message
                         std::cout << "\033[1;31m" << e.what() << "\033[0m" << std::endl;
                         _send(fd, "HTTP/1.1 " + std::to_string(e.get_error_status()) + " " + statusCodes()[e.get_error_status()] + "\n");
                     }
--- srcs/main/MyWebServer.hpp
+++ srcs/main/MyWebServer.hpp
@@ -15,12 +15,13 @@ class MyWebServer {
         ~MyWebServer();
 
         // Sockets
-        void close_socket (const char * error_msg, int sock);
+        void error_socket (const char * error_msg, int sock);
         int new_socket (const char *host, int port);
         void _bind_all (const char *host = "127.0.0.1");
 
         // Connect
         int accept_client(int server_sock);
+        void remove_client(int client_sock);
 
         int _send(int sock, std::string msg);
         std::string _recv(int sock);
@@ -32,7 +33,7 @@ class MyWebServer {
     private:
         fd_set              current_sockets; // list of all sockets.
         std::map<int, int>  client_server; // client_sock : server_sock
-        std::queue<int>     queue_clients;
+        std::list<int>      queue_clients;
         std::vector<int>    server_sockets;
         std::vector<Server> servers;
 };
